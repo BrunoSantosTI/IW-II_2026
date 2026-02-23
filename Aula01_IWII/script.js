5a931ef635d5a5cae1b238aec87a5fd6999ebf74
BrunoSantosTI <brunnof.santosti@gmail.com>
2026-02-23 08:46:37 -0300

    Add files via upload

---
+++ Aula01_IWII/script.js
@@ -0,0 +1,3 @@
+console.log('Hello World');
+var nome = "Bruno";
+console.log(nome)
---
+++ Aula01_IWII/variaveis.js
@@ -0,0 +1,25 @@
+var variavel1 = 'Bruno';
+var variavel2 = 'Brunão';
+var variavelIdade = 15;
+var souLindo = true;
+var  variavelIndefinida;
+
+console.log(variavel1);
+console.log(variavel2);
+console.log(variavelIdade);
+console.log(souLindo);
+console.log(variavelIndefinida);
+
+var a = 15;
+var b = 55;
+
+var tr = (a == 15);
+var fl = (a == b);
+
+// fl = false e tr = true
+
+var int1 = tr + 1;
+var int2 = fl + 1;
+
+console.log(int1);
+console.log(int2);
